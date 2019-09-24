9d4e5a61499dcc26a44e5a7d46d20c20a781048f
Oweis <oweisyahya@gmail.com>
2019-09-24 11:32:03 +0100

    Scrap monster (not done) + use services in place of repositories

--- src/main/java/com/dofus/tools/scrapper/impl/BestiaireScrapper.java
+++ src/main/java/com/dofus/tools/scrapper/impl/BestiaireScrapper.java
@@ -1,10 +1,8 @@
 package com.dofus.tools.scrapper.impl;
 
 import com.dofus.tools.mesarchi.model.*;
-import com.dofus.tools.mesarchi.repository.AreaRepository;
-import com.dofus.tools.mesarchi.repository.FamilyGroupRepository;
-import com.dofus.tools.mesarchi.repository.FamilyRepository;
-import com.dofus.tools.mesarchi.repository.TypeRepository;
+import com.dofus.tools.mesarchi.repository.*;
+import com.dofus.tools.mesarchi.service.*;
 import com.dofus.tools.scrapper.IScrapper;
 import com.google.common.collect.HashBasedTable;
 import com.google.common.collect.Table;
@@ -17,8 +15,14 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.*;
+import java.util.stream.Collectors;
 
 @Component
 public class BestiaireScrapper implements IScrapper {
@@ -31,17 +35,24 @@ public class BestiaireScrapper implements IScrapper {
     public static final String PORTUGUESE = "pt";
     public static final String DOFUS_URL = "https://www.dofus.com";
     public static final String BESTIAIRE_URL = "https://www.dofus.com/fr/mmorpg/encyclopedie/monstres";
+    // This user agent is for if the server wants real humans to visit
+    public static final String USER_AGENT_MOZILLA = "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:25.0) Gecko/20100101 Firefox/25.0";
 
     private static final Logger LOGGER = LoggerFactory.getLogger(BestiaireScrapper.class);
 
+
     @Autowired
-    AreaRepository areaRepository;
+    AreaService areaService;
     @Autowired
-    TypeRepository typeRepository;
+    TypeService typeService;
     @Autowired
-    FamilyGroupRepository familyGroupRepository;
+    FamilyGroupService familyGroupService;
     @Autowired
-    FamilyRepository familyRepository;
+    FamilyService familyService;
+    @Autowired
+    MonsterService monsterService;
+
+
     Random random = new Random();
 
     private static String getLocalizedURL(String url, String sourceLanguage, String targetLanguage) {
@@ -71,10 +82,58 @@ public class BestiaireScrapper implements IScrapper {
                 .replace(encyclopediaMap.get(sourceLanguage).monster, encyclopediaMap.get(targetLanguage).monster);
     }
 
+    public static void downloadImage(String search, String path) throws IOException {
+
+        // This will get input data from the server
+        InputStream inputStream = null;
+
+        // This will read the data from the server;
+        OutputStream outputStream = null;
+
+        try {
+            // This will open a socket from client to server
+            URL url = new URL(search);
+
+
+            // This socket type will allow to set user_agent
+            URLConnection con = url.openConnection();
+
+            // Setting the user agent
+            con.setRequestProperty("User-Agent", USER_AGENT_MOZILLA);
+
+            // Requesting input data from server
+            inputStream = con.getInputStream();
+
+            // Open local file writer
+            outputStream = new FileOutputStream(path);
+
+            // Limiting byte written to file per loop
+            byte[] buffer = new byte[2048];
+
+            // Increments file size
+            int length;
+
+            // Looping until server finishes
+            while ((length = inputStream.read(buffer)) != -1) {
+                // Writing data
+                outputStream.write(buffer, 0, length);
+            }
+        } catch (Exception ex) {
+            LOGGER.debug("ERROR ...");
+        }
+
+        // closing used resources
+        // The computer will not be able to use the image
+        // This is a must
+
+        outputStream.close();
+        inputStream.close();
+    }
+
     @Override
     public void populateDatabase() throws IOException, InterruptedException {
-        //populateDatabaseFilters();
-        populateDatabaseMonstres();
+        populateDatabaseFilters();
+        //populateDatabaseMonstres();
     }
 
     public void populateDatabaseFilters() throws IOException {
@@ -87,27 +146,25 @@ public class BestiaireScrapper implements IScrapper {
         Table areaTable = retrieveListFilters(areaDataname);
         Map<String, Map<String, String>> areaMap = areaTable.rowMap();
         for (String row : areaMap.keySet()) {
-            Area area = new Area(areaMap.get(row));
-            LOGGER.info(area.getName().get("en"));
-            areaRepository.save(area);
+            areaService.addArea(new Area(areaMap.get(row)));
         }
 
         Table familyGroupTable = retrieveListFilters(familyGroupDataname);
         Map<String, Map<String, String>> familyGroupMap = familyGroupTable.rowMap();
         for (String row : familyGroupMap.keySet()) {
-            familyGroupRepository.save(new FamilyGroup(familyGroupMap.get(row)));
+            familyGroupService.addFamilyGroup(new FamilyGroup(familyGroupMap.get(row)));
         }
 
         Table familyTable = retrieveListFilters(familyDataname);
         Map<String, Map<String, String>> familyMap = familyTable.rowMap();
         for (String row : familyMap.keySet()) {
-            familyRepository.save(new Family(familyMap.get(row)));
+            familyService.addFamily(new Family(familyMap.get(row)));
         }
 
         Table typeTable = retrieveListFilters(typeDataname);
         Map<String, Map<String, String>> typeMap = typeTable.rowMap();
         for (String row : typeMap.keySet()) {
-            typeRepository.save(new Type(typeMap.get(row)));
+            typeService.addType(new Type(typeMap.get(row)));
         }
     }
 
@@ -128,7 +185,7 @@ public class BestiaireScrapper implements IScrapper {
         String url = getLocalizedURL(BESTIAIRE_URL, FRENCH, language);
         LOGGER.info("URL: {}", url);
         Document doc = Jsoup.connect(url)
-                .userAgent("Mozilla")
+                .userAgent(USER_AGENT_MOZILLA)
                 .timeout(random.nextInt(5000) + 10000)
                 .get();
         Element familyGroupDivElement = doc.getElementsByAttributeValue("data-name", dataname).get(0);
@@ -147,16 +204,17 @@ public class BestiaireScrapper implements IScrapper {
         return tableByLanguage;
     }
 
-    public void populateDatabaseMonstres() throws IOException, InterruptedException {
+    public void populateDatabaseMonstres() throws IOException {
         //Do nothing, will be developed soon
         int bestiairePagesNumber = 19;
-        List<String> monsterPathList = new ArrayList<>();
 
         for (int i = 1; i <= bestiairePagesNumber; i++) {
             Document doc = Jsoup.connect(BESTIAIRE_URL)
                     .data("size", "96")
                     .data("display", "table")
                     .data("page", String.valueOf(i))
+                    .userAgent(USER_AGENT_MOZILLA)
+                    .timeout(random.nextInt(5000) + 10000)
                     .get();
 
             Elements monstersTables = doc.getElementsByClass("ak-table ak-responsivetable");
@@ -165,36 +223,87 @@ public class BestiaireScrapper implements IScrapper {
                 Elements monsters = monsterTable.getElementsByClass("ak-bg-odd");
                 monsters.addAll(monsterTable.getElementsByClass("ak-bg-even"));
 
-                for (Element monster : monsters) {
-                    String monsterPath = monster.getElementsByTag("td").get(1).getElementsByTag("span").get(0).getElementsByTag("a").get(0).attr("href");
-                    retrieveMonsterDetailsByUrlAndLanguage(DOFUS_URL + monsterPath, ENGLISH);
-                    monsterPathList.add(monsterPath);
+                for (Element monsterElement : monsters) {
+                    String monsterPath = monsterElement.getElementsByTag("td").get(1).getElementsByTag("span").get(0).getElementsByTag("a").get(0).attr("href");
+                    Monster monster = scrapMonster(DOFUS_URL + monsterPath);
+                    monsterService.addMonster(monster);
                 }
             }
         }
-        LOGGER.info("Size: {}.",    monsterPathList.size());
     }
 
+    public Monster scrapMonster(String monsterUrl) throws IOException {
+        //set french name and the other monster information
+        Monster monster = initializeMonster(monsterUrl);
+        //set name in other languages
+        monster.getName().put(ENGLISH, scrapMonsterNameByLanguage(monsterUrl, ENGLISH));
+        monster.getName().put(SPANISH, scrapMonsterNameByLanguage(monsterUrl, SPANISH));
+        monster.getName().put(GERMAN, scrapMonsterNameByLanguage(monsterUrl, GERMAN));
+        monster.getName().put(PORTUGUESE, scrapMonsterNameByLanguage(monsterUrl, PORTUGUESE));
+        monster.getName().put(ITALIAN, scrapMonsterNameByLanguage(monsterUrl, ITALIAN));
 
+        return monster;
+    }
 
-    public  void retrieveMonsterDetailsByUrlAndLanguage(String url, String language) throws IOException {
-        String localizedURL = getLocalizedURL(url, FRENCH, language);
+    public Monster initializeMonster(String urlMonster) throws IOException {
 
-        Document docMonster = Jsoup.connect(localizedURL)
-                .userAgent("Mozilla")
+        Document docMonster = Jsoup.connect(urlMonster)
+                .userAgent(USER_AGENT_MOZILLA)
                 .timeout(random.nextInt(5000) + 10000)
                 .get();
         Element docMonsterDetail = docMonster.getElementsByClass("ak-container ak-panel-stack ak-glue").get(0);
         String name = docMonsterDetail.getElementsByClass("ak-return-link").get(0).text();
 
-        String picture = docMonsterDetail.getElementsByClass("ak-encyclo-detail-illu").get(0).getElementsByTag("img").get(0).attr("src");
+        String picture = docMonsterDetail.getElementsByClass("ak-encyclo-detail-illu").get(0).getElementsByTag("img").get(0).attr("data-src");
         String types = docMonsterDetail.getElementsByClass("ak-encyclo-detail-type").get(0).getElementsByTag("span").get(0).text();
         String level = docMonsterDetail.getElementsByClass("ak-encyclo-detail-level").get(0).text();
-        String zones = docMonsterDetail.getElementsByClass("ak-container ak-panel").get(3).getElementsByClass("ak-panel-content").get(0).text();
 
-        LOGGER.info("name: {}, picture: {}, types: {}, level: {}, zones: {}.", name, picture, types, level, zones);
+
+        String areas = docMonsterDetail.getElementsByClass("ak-container ak-panel").get(3).getElementsByClass("ak-panel-content").get(0).text();
+        if (areas.contains("Spell animations")) {
+            areas = "";
+        }
+
+        List<String> areasList = Arrays.stream(areas.split(","))
+                .map(areasElement -> areasElement.trim())
+                .collect(Collectors.toList());
+
+
+        String levelMin = "";
+        String levelMax = "";
+        String[] levelList = level.replace("Level:", "").replace("to", ",").split(",");
+        if (levelList.length == 2) {
+            levelMin = levelList[0];
+            levelMax = levelList[1];
+        } else {
+            levelMin = levelList[0];
+            levelMax = levelList[0];
+        }
+
+
+//        downloadImage(picture, "C:/Workspace/Java/Picture/"+name+".jpg");
+        System.out.println("Types: " + types + ", levelMin: " + levelMin + ", levelMax: " + levelMax + " areas: " + areas + ".");
+        //LOGGER.info("name: {}, picture: {}, types: {}, level: {}, areas: {}.", name, picture, types, level, areas);
+        Monster monster = new Monster();
+        monster.getName().put(ENGLISH, name);
+        monster.setLevelMax(levelMax);
+        monster.setLevelMax(levelMin);
+        monster.setPicture(picture);
+        //monster.setGame();
+
+
+        return new Monster();
     }
 
+    public String scrapMonsterNameByLanguage(String monsterUrl, String language) throws IOException {
+        Document docMonster = Jsoup.connect(getLocalizedURL(monsterUrl, FRENCH, PORTUGUESE))
+                .userAgent(USER_AGENT_MOZILLA)
+                .timeout(random.nextInt(5000) + 10000)
+                .get();
+        Element docMonsterDetail = docMonster.getElementsByClass("ak-container ak-panel-stack ak-glue").get(0);
+        return docMonsterDetail.getElementsByClass("ak-return-link").get(0).text();
+
+    }
 }
 
 
